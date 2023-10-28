8ef1c711123e609750f164c337be23555a31bdc9
Karol Małecki <malecki.karol@outlook.com>
2023-10-28 16:45:44 +0200

    introduce theme provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,35 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
 } from "react-router-dom";
 
 import News from "./newsfeed.js";
 import Projects from "./projects.js";
+import {ThemeProvider} from "@mui/material";
+import {createTheme} from "@mui/material";
 
 const router = createBrowserRouter([{
     path: "/",
     element: <Projects/>,
-  },
-  {
-    path: "news",
-    element: <News />,
-  },
+},
+    {
+        path: "news",
+        element: <News/>,
+    },
 ]);
 
+const theme = createTheme({
+    palette: {
+        primary: "#b30000"
+    }
+});
+
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+        <RouterProvider router={router}/>
+    </ThemeProvider>
 );
