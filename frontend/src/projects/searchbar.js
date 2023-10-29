49f256593e48305d6f839c8125c38e6f5090953f
Karol Małecki <malecki.karol@outlook.com>
2023-10-29 04:40:45 +0100

    make project type selector more readable

--- frontend/src/projects/searchbar.js
+++ frontend/src/projects/searchbar.js
@@ -3,6 +3,7 @@ import Grid2 from "@mui/material/Unstable_Grid2";
 import {Box, Chip, Stack, TextField, ToggleButton, ToggleButtonGroup, Typography} from "@mui/material";
 import {FaX, FaXmark} from "react-icons/fa6";
 import TagSelector from "../tagselector";
+import {Done} from "@mui/icons-material";
 
 export default function SearchBar({
                                       tags,
@@ -16,6 +17,9 @@ export default function SearchBar({
         setSelectedProjectTypes(newProjectTypes);
     };
 
+    const areOfficialEnabled = selectedProjectTypes.includes("Official");
+    const arePropositionsEnabled = selectedProjectTypes.includes("Proposition");
+
     return <Grid2 sx={{mt: 2, mb: 2}} container spacing={2}>
         <Grid2 item sm={12} lg={6}>
             <TextField sx={{"width": "100%"}} label={"Nazwa projektu..."} variant={"filled"}
@@ -26,12 +30,12 @@ export default function SearchBar({
         </Grid2>
         <Grid2 sx={{alignSelf: "flex-end"}} item sm={12} lg={6}>
             <ToggleButtonGroup sx={{width: "100%"}} value={selectedProjectTypes} onChange={handleProjectTypes}
-                               color={"primary"}>
+                               color={"secondary"} >
                 <ToggleButton value={"Official"} sx={{flexGrow: "1"}}>
-                    <Typography variant={"body1"}>Projekty Samorządowe</Typography>
+                    <Typography variant={"body1"}>Projekty Samorządowe {areOfficialEnabled ? <Done sx={{pt: 1}} /> : ""}</Typography>
                 </ToggleButton>
                 <ToggleButton value={"Proposition"} sx={{flexGrow: "1"}}>
-                    <Typography variant={"body1"}>Propozycje Obywatelskie</Typography>
+                    <Typography variant={"body1"}>Propozycje Obywatelskie {arePropositionsEnabled ? <Done sx={{pt: 1}} /> : ""}</Typography>
                 </ToggleButton>
             </ToggleButtonGroup>
         </Grid2>
